559ebafd4c1ba0e6e45b8a45d7cb5e0e656c25e0
TranLe2403 <trantran.0324@gmail.com>
2021-03-24 01:10:06 +0200

    update: add images representing for each product item

---
+++ src/images/images.js
@@ -0,0 +1,9 @@
+import cocacola from "./cocacola.jpeg";
+import haribo from "./haribo.jpeg";
+import mountaindew from "./mountaindew.jpeg";
+import oreo from "./oreo.jpeg";
+import mm from "./m&m.jpeg";
+
+const allImages = [cocacola, haribo, mountaindew, oreo, mm];
+
+export default allImages;
