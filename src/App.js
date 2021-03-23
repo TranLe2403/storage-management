6884a2f581b3bd9cef359978be9633408215c113
TranLe2403 <trantran.0324@gmail.com>
2021-03-24 01:10:51 +0200

    feat: display all items and add stylings for each and the whole list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import productData from "./products.json";
+import {
+  Box,
+  Grid,
+  Image,
+  Text,
+  Stack,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
+import allImages from "./images/images";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading color="teal" textAlign="center" marginTop={4}>
+        MY STORAGE
+      </Heading>
+
+      <Grid templateColumns="repeat(3, 1fr)" gap={4}>
+        {productData.products.map((item) => (
+          <Box
+            key={item.id}
+            bg="tomato"
+            width="400px"
+            p={6}
+            color="white"
+            margin="30px auto"
+            padding="30px"
+            borderRadius="lg"
+            shadow="2xl"
+          >
+            <Image
+              src={allImages[Math.floor(Math.random() * allImages.length)]}
+              alt={item.productName}
+              width="100%"
+              height="200px"
+              borderRadius="lg"
+            />
+            <Text fontSize="20px">
+              <strong>{item.productName}</strong>
+            </Text>
+            <p>
+              Price: €{item.price} | Quantity: {item.quantity}
+            </p>
+            <p>Unit: {item.unit}</p>
+            <p>Brand: {item.brand}</p>
+            <p>Category: {item.category}</p>
+            <Stack direction="row" spacing={4} align="center" marginTop={4}>
+              <Button
+                colorScheme="teal"
+                variant="solid"
+                width="50%"
+                height="30px"
+                borderRadius="full"
+              >
+                Edit
+              </Button>
+              <Button
+                colorScheme="teal"
+                variant="solid"
+                width="50%"
+                height="30px"
+                borderRadius="full"
+              >
+                Delete
+              </Button>
+            </Stack>
+          </Box>
+        ))}
+      </Grid>
     </div>
   );
 }
